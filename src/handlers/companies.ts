a9ea5ed0b78dffdb310be96a11a4cc948b43d1a8
botprzemek <info@botprzemek.pl>
2025-11-04 21:26:33 +0100

    fix: format code for consistency in companies.ts

--- src/handlers/companies.ts
+++ src/handlers/companies.ts
@@ -22,12 +22,12 @@ export default new Hono<{ Bindings: Bindings }>()
     const { sort, order, size, page } = c.req.query();
     const filter = c.req.queries("filter");
 
-    const data = await get(c.env.D1,organization_id, { sort, order, size, page, filter });
+    const data = await get(c.env.D1, organization_id, { sort, order, size, page, filter });
 
     return c.json({ data });
   })
   .get("/:name", validateHeader, async (c) => {
-    const {organization_id } = c.req.valid("header");
+    const { organization_id } = c.req.valid("header");
 
     if (!organization_id) {
       throw new HTTPException(400);
@@ -36,7 +36,7 @@ export default new Hono<{ Bindings: Bindings }>()
     const { name } = c.req.param();
     const filter = c.req.queries("filter");
 
-    const data = await getByName(c.env.D1,organization_id, name, { filter });
+    const data = await getByName(c.env.D1, organization_id, name, { filter });
 
     if (!data) {
       throw new HTTPException(404);
@@ -45,11 +45,11 @@ export default new Hono<{ Bindings: Bindings }>()
     return c.json({ data });
   })
   .post("/", validateHeader, validateBody, async (c) => {
-    const {organization_id } = c.req.valid("header");
+    const { organization_id } = c.req.valid("header");
     const body = c.req.valid("json");
 
     try {
-      const data = await create(c.env.D1,organization_id, body);
+      const data = await create(c.env.D1, organization_id, body);
 
       return c.json({ data }, 201);
     } catch (e: any) {
@@ -63,10 +63,10 @@ export default new Hono<{ Bindings: Bindings }>()
     }
   })
   .put("/:name", validateHeader, validateBody, async (c) => {
-    const {organization_id } = c.req.valid("header");
+    const { organization_id } = c.req.valid("header");
     const { name } = c.req.param();
 
-    const check = await getByName(c.env.D1,organization_id, name);
+    const check = await getByName(c.env.D1, organization_id, name);
 
     if (!check) {
       throw new HTTPException(404);
@@ -74,15 +74,15 @@ export default new Hono<{ Bindings: Bindings }>()
 
     const body = c.req.valid("json");
     const entries = Object.entries(body)
-      .filter(([key, value]) => value !== check[key as keyof Omit<Company, "cid" | "tid">])
+      .filter(([key, value]) => value !== check[key as keyof Omit<Company, "id" | "organization_id">])
       .map(([key, value]) => [`${key} = ?`, value]);
 
     if (!entries.length) {
-      return c.redirect(`/v1/tenants/${name}`, 303);
+      return c.redirect(`/v1/companies/${name}`, 303);
     }
 
     try {
-      const data = await updateByName(c.env.D1,organization_id, name, entries);
+      const data = await updateByName(c.env.D1, organization_id, name, entries);
 
       if (!data) {
         throw new HTTPException(404);
@@ -90,16 +90,16 @@ export default new Hono<{ Bindings: Bindings }>()
 
       return c.json({ data });
     } catch (e: any) {
-      if (e.message === "D1_ERROR: UNIQUE constraint failed: tenants.slug: SQLITE_CONSTRAINT") {
+      if (e.message === "D1_ERROR: UNIQUE constraint failed: organizations.slug: SQLITE_CONSTRAINT") {
         throw new HTTPException(409);
       }
     }
   })
   .delete("/:name", validateHeader, async (c) => {
-    const {organization_id } = c.req.valid("header");
+    const { organization_id } = c.req.valid("header");
     const { name } = c.req.param();
 
-    const data = await deleteByName(c.env.D1,organization_id, name);
+    const data = await deleteByName(c.env.D1, organization_id, name);
 
     if (!data) {
       throw new HTTPException(404);
